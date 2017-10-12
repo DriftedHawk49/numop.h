2b34d2b5aaf7835d10f9b8ecfbfd2ea434fec236
Neelesh Chandola <neelesh.chandola@outlook.com>
2017-10-12 18:56:35 +0530

    Use templates for writing generic functions (2/2)

--- numop.h
+++ numop.h
@@ -4,40 +4,31 @@
 #include<vector>
 //include Your Functions Here - 
 
-int number_of_digits(int);//--------For Counting Digits in a integer type (done)
-int number_of_digits(long);//------For Counting Digits in a Long Integer (done)
-int number_of_digits(long long);//-----For COunting Number of digits in a Long Long integer (done)
+template<typename t>
+int number_of_digits(t);//--------For Counting Digits in a integer type (done)
 
-int ifpallin(int);// For Checking an integer for a Pallindrome (done)
-int ifpallin(long);// For Checking a long integer for a Pallindrome (done)
-int ifpallin(long long);// For Checking a long long integer for a Pallindrome (done)
+template<typename t>
+int ifpallin(t);// For Checking an integer for a Pallindrome (done)
 
-std::vector<int> dig(int);//Returns a Vector Containg all the individual digits of that integer in that order(done)
-std::vector<int> dig(long);//Returns a Vector Containg all the individual digits of that long integer in that order (done)
-std::vector<int> dig(long long);//Returns a Vector Containg all the individual digits of that long long integer in that order(done)
+template<typename t>
+std::vector<int> dig(t);//Returns a Vector Containg all the individual digits of that integer in that order(done)
 
-std::vector<int> digrev(int);//Returns a Vector Containg all the individual digits of that integer in reverse order(done)
-std::vector<int> digrev(long);//Returns a Vector Containg all the individual digits of that long integer in reverse order(done)
-std::vector<int> digrev(long long);//Returns a Vector Containg all the individual digits of that long long integer in reverse order(done)
+template<typename t>
+std::vector<int> digrev(t);//Returns a Vector Containg all the individual digits of that integer in reverse order(done)
 
-int maxdig(int);// Returns the maximum digit present in that number
-int maxdig(long);
-int maxdig(long long);
+template<typename t>
+int maxdig(t);// Returns the maximum digit present in that number
 
-int mindig(int);
-int mindig(long);
-int mindig(long long);
+template<typename t>
+int mindig(t);
 
-std::vector<int> bincal(int);//done
-std::vector<int> bincal(long);//done
-std::vector<int> bincal(long long);//done
+template<typename t>
+std::vector<int> bincal(t);//done
 
-int oddeven(int);//done
-int oddeven(long);//done
-int oddeven(long long);//done
+template<typename t>
+int oddeven(t);//done
 
-int prime_check(int);//done
-int prime_check(long);//done
-int prime_check(long long);//done
+template<typename t>
+int prime_check(t);//done
 
 #endif
